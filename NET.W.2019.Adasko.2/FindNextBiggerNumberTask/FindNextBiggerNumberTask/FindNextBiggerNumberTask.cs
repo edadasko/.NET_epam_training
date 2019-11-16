466540e5dd67452a123ff557251c356b13f8d6f6
edadasko <edadasko@gmail.com>
2019-11-17 00:22:18 +0300

    add second task

---
+++ NET.W.2019.Adasko.2/FindNextBiggerNumberTask/FindNextBiggerNumberTask/FindNextBiggerNumberTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FindNextBiggerNumberTask
+{
+    public static class FindNextBiggerNumber
+    {
+        public static int Find(int num)
+        {
+            var strNum = num.ToString();
+
+            var indexesForSwapping = FindOptimalChangingIndexes(strNum);
+
+            if (indexesForSwapping == null)
+                return -1;
+
+            var numArray = strNum.ToCharArray();
+            Swap(ref numArray[indexesForSwapping.Item1], ref numArray[indexesForSwapping.Item2]);
+            Array.Sort(numArray, indexesForSwapping.Item1 + 1, numArray.Length - indexesForSwapping.Item1 - 1);
+            return Convert.ToInt32(string.Join("", numArray));
+        }
+
+        private static Tuple<int, int> FindOptimalChangingIndexes(string num)
+        {
+            for (int i = num.Length - 2; i >= 0; i--)
+            {
+                int nextBiggerDigit = FindNextBiggerDigit(i, num);
+                if (nextBiggerDigit != -1)
+                    return new Tuple<int, int>(i, nextBiggerDigit);
+            }
+            return null;
+        }
+
+        private static void Swap(ref char a, ref char b)
+        {
+            char temp = a; a = b; b = temp;
+        }
+
+        private static int FindNextBiggerDigit(int index, string num)
+        {
+            int currentDigit = Convert.ToInt32(num[index]);
+            int nextBiggerDigitIndex = -1;
+            for (int i = index + 1; i < num.Length; i++)
+            {
+                int nextDigit = Convert.ToInt32(num[i]);
+                if (nextDigit > currentDigit &&
+                    (nextBiggerDigitIndex == -1 || nextDigit < num[nextBiggerDigitIndex]))
+                    nextBiggerDigitIndex = i;
+            }
+            return nextBiggerDigitIndex;
+               
+        }
+    }
+}
---
+++ NET.W.2019.Adasko.2/FindNextBiggerNumberTask/FindNextBiggerNumberTest/FindTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using FindNextBiggerNumberTask;
+
+namespace FindNextBiggerNumberTest
+{
+    public class Tests
+    {
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(2354, ExpectedResult = 2435)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+        public int TestFindNext(int num) =>
+            FindNextBiggerNumber.Find(num);
+    }
+}
