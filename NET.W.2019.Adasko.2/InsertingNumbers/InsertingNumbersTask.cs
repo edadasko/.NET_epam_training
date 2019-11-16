0a0fb5632d7b109f2931d2128b659dc7e00a2f50
edadasko <edadasko@gmail.com>
2019-11-16 22:07:26 +0300

    add first task of day2

---
+++ NET.W.2019.Adasko.2/InsertingNumbers/InsertingNumbersTask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace InsertingNumbers
+{
+    public static class InsertingNumbersTask
+    {
+        private const int numOfBits = 32;
+        private const uint mask = uint.MaxValue;
+
+        /// <summary>
+        /// Inserts bits of one integer number to another.
+        /// Segment of [i, j] bits of the first number will be replaced
+        /// by the first (j - i) bits of the second number.
+        /// Numbers of bits for inserting should be passed as parameters.
+        /// </summary>
+        /// <param name="targetNum">
+        /// The number in which you want insert bits of another number.
+        /// </param>
+        /// <param name="insertNum">
+        /// The number which provides bits for inserting.
+        /// </param>
+        /// <param name="i">
+        /// The first index of segment for inserting of targetNum.
+        /// </param>
+        /// <param name="j">
+        /// The last index of segment for inserting of targetNum.
+        /// </param>
+        /// <exception cref="OverflowException">
+        /// Thrown when the resulting number contains 32 '1' bits.
+        /// This number does not fit in integer range.
+        /// </exception>
+        /// /// <exception cref="ArgumentException">
+        /// Thrown when passed indexes are incorrect.
+        /// </exception>
+        public static int InsertNumber(int targetNum, int insertNum, int i, int j)
+        {
+            if (i > j || i < 0 || j < 0 || i > numOfBits - 1 || j > numOfBits - 1)
+                throw new ArgumentException();
+
+            uint maskForInsertNum = mask >> (numOfBits - 1 - j);
+            insertNum = (int)((insertNum << i) & maskForInsertNum);
+
+            uint maskForTargetNum = (mask << j) | (mask >> numOfBits - 1 - i);
+            targetNum = (int)(maskForTargetNum & targetNum);
+
+            if ((uint)(insertNum | targetNum) == uint.MaxValue)
+                throw new OverflowException();
+
+            return insertNum | targetNum;
+        }
+    }
+}
---
+++ NET.W.2019.Adasko.2/InsertingNumbersTest/InsertingTests.cs
@@ -0,0 +1,50 @@
+using InsertingNumbers;
+using NUnit.Framework;
+
+namespace InsertingNumbersTest
+{
+    public class InsertingTests
+    {
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+
+        [TestCase(0b00010000_00101100_11110001_10000111,
+                  0b00000000_00000000_11000011_10000011, 24, 31,
+                  ExpectedResult =
+                  unchecked((int)
+                  0b10000011_00101100_11110001_10000111))]
+
+        [TestCase(0b00010000_00101100_11110001_10000111,
+                  unchecked((int)
+                  0b10000000_00000000_11000011_10000011), 0, 31,
+                  ExpectedResult =
+                  unchecked((int)
+                  0b10000000_00000000_11000011_10000011))]
+
+        [TestCase(0b00010000_00101100_11110001_10000111,
+                  unchecked((int)
+                  0b10000000_00000000_11000011_10000011), 0, 31,
+                  ExpectedResult =
+                  unchecked((int)
+                  0b10000000_00000000_11000011_10000011))]
+
+        [TestCase(unchecked((int)0b11110000_00101100_11110001_10000111),
+                  unchecked((int)0b10011000_11111000_10100011_10111011), 6, 24,
+                  ExpectedResult =
+                  unchecked((int)0b11110000_00101000_11101110_11000111))]
+
+        public long InsertTests(int target, int insert, int i, int j) =>
+            InsertingNumbersTask.InsertNumber(target, insert, i, j);
+
+        [Test]
+        public void InsertExceptionTest()
+        {
+            Assert.Throws<System.OverflowException>(() =>
+                InsertingNumbersTask.InsertNumber(
+                    0b00000000_00000000_11111111_11111111,
+                    0b00000000_00000000_11111111_11111111, 16, 31));
+        }
+            
+    }
+}
