4664566d72aa14322e0532546d5286d583e3b4c9
edadasko <edadasko@gmail.com>
2019-11-16 22:30:37 +0300

    add MS Unit tests for first task

---
+++ NET.W.2019.Adasko.2/InsertingNumbersMsUnitTest/InsertingTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InsertingNumbers;
+using System;
+
+namespace InsertingNumbersMsUnitTest
+{
+    [TestClass]
+    public class InsertingMsUnitTests
+    {
+        [TestMethod]
+        public void ExceptionTest()
+        {
+            Assert.ThrowsException<OverflowException>(() => InsertingNumbersTask.InsertNumber(
+                0b00000000_00000000_11111111_11111111,
+                0b00000000_00000000_11111111_11111111,
+                16, 31));
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Assert.AreEqual(InsertingNumbersTask.InsertNumber(15, 15, 0, 0), 15);
+            Assert.AreEqual(InsertingNumbersTask.InsertNumber(8, 15, 0, 0), 9);
+            Assert.AreEqual(InsertingNumbersTask.InsertNumber(8, 15, 3, 8), 120);
+        }
+    }
+}
