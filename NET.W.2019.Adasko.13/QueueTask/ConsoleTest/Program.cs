1e68f55d57d5796875b6f4dedc4bf47af072afe4
edadasko <edadasko@gmail.com>
2019-12-18 00:11:31 +0300

    change directory name

---
+++ NET.W.2019.Adasko.13/QueueTask/ConsoleTest/Program.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="EpamTraining">
+//     All rights reserved.
+// </copyright>
+// <author>Eduard Adasko</author>
+//-----------------------------------------------------------------------
+
+namespace ConsoleTest
+{
+	using System;
+	using QueueTask;
+
+	/// <summary>
+	/// Tests working with Queue class.
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// Tests queue.
+		/// </summary>
+		public static void Main()
+		{
+			Queue<int> intQ = new Queue<int>();
+
+			for (int i = 0; i < 10; i++)
+			{
+				intQ.Push(i);
+			}
+
+			foreach (var a in intQ)
+			{
+				Console.WriteLine(a);
+			}
+
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(intQ.Pop());
+			}
+
+			Queue<string> stringQ = new Queue<string>();
+			stringQ.Push("12345");
+			stringQ.Push("98765");
+			stringQ.Push("123");
+			stringQ.Pop();
+
+			foreach (var a in stringQ)
+			{
+				Console.WriteLine(a);
+			}
+		}
+	}
+}
