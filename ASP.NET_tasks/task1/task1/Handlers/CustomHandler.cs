542af156fa764badb72db7d5762e53db4129e3d1
edadasko <edadasko@gmail.com>
2020-01-20 23:22:07 +0300

    add custom http handler

---
+++ ASP.NET_tasks/task1/task1/Handlers/CustomHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace task1.Handlers
+{
+    public class CustomHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<h1>Custom Handler</h1>");
+        }
+    }
+}
