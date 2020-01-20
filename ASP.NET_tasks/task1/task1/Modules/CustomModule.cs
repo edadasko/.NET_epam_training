060b20459be4230f19fc18cc7e347286f99e16ad
edadasko <edadasko@gmail.com>
2020-01-21 01:00:31 +0300

    add custom module for logging requests

---
+++ ASP.NET_tasks/task1/task1/Modules/CustomModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace task1.Modules
+{
+    public class CustomModule : IHttpModule
+    {
+        private const string FilePath = "log.txt";
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(this.HandleBeginRequest);
+            context.EndRequest += new EventHandler(this.HandleEndRequest);
+        }
+
+        public void HandleEndRequest(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(HttpContext.Current.Server.MapPath(FilePath), true))
+            {
+                sw.WriteLine("End request called at " + DateTime.Now.ToString());
+            }
+        }
+
+        public void HandleBeginRequest(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(HttpContext.Current.Server.MapPath(FilePath), true))
+            {
+                sw.WriteLine("Begin request called at " + DateTime.Now.ToString());
+            }
+        }
+    }
+}
